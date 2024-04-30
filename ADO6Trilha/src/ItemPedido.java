f4de173ecacd2f931c8a46af28956f9ba0d34b7a
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-04-30 17:45:00 -0300

    Implementada classe ItemPedido com métodos getters e setters para nome e preço

---
+++ ADO6Trilha/src/ItemPedido.java
@@ -0,0 +1,28 @@
+public class ItemPedido {
+
+    private String nome;
+    private double preco;
+
+    public ItemPedido(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
