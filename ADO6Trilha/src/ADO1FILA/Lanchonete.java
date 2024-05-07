e750466e678b9d99727f6ab975fe4cdcb80d870f
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-05-07 15:47:50 -0300

    Adicionado métodos para adicionar, chamar próximo e listar pedidos na classe Lanchonete

---
+++ ADO6Trilha/src/ADO1FILA/Lanchonete.java
@@ -0,0 +1,33 @@
+package ADO1FILA;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Lanchonete {
+
+    private Queue<ItemPedido> pedidos = new ArrayDeque<>();
+
+    public void adicionarPedido(ItemPedido item) {
+        pedidos.offer(item);
+        System.out.println("O pedido foi realizado com sucesso!");
+        System.out.println("Nome: " + item.getNome());
+        System.out.println("Preço: R$" + item.getPreco());
+    }
+
+    public void proximoPedido() {
+        if (!pedidos.isEmpty()) {
+            ItemPedido proximo = pedidos.poll();
+            System.out.println("Chamando próximo pedido de um " + proximo.getNome());
+        } else {
+            System.out.println("Não há pedidos na fila.");
+        }
+    }
+
+    public void listarPedidos() {
+        System.out.println("--- Lista de Pedidos ---");
+        for (ItemPedido itemPedido : pedidos) {
+            System.out.println("Nome: " + itemPedido.getNome());
+            System.out.println("Preço: R$" + itemPedido.getPreco());
+        }
+
+    }
+}
